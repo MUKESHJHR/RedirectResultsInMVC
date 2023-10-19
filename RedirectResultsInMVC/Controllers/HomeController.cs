d64fea5c46271dd2983f71e554a79b1ada53ce87
mukesh kumar <mukesh.1006jhr@outlook.com>
2023-10-20 04:46:28 +0530

    Add project files.

---
+++ RedirectResultsInMVC/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedirectResultsInMVC.Models;
+using System.Diagnostics;
+
+namespace RedirectResultsInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Redirect Result in ASP.NET Core MVC
+        //public RedirectResult Index()
+        //{
+
+        //    //return new RedirectResult("https://dotnettutorials.net");
+
+        //    //Using RedirectResult Helper Method
+        //    return Redirect("https://dotnettutorials.net");
+        //}
+
+        //RedirectToRouteResult
+
+        //RedirectToActionResult in ASP.NET Core MVC
+        //public RedirectToRouteResult Index()
+        //{
+        //    // Perform some logic
+        //    // Redirect to a different route
+        //    // You need to specify the Route Name, not the Route Pattern
+        //    //return RedirectToRoute("AboutRoute", new { name = "Mukesh Kumar" });
+
+        //    //Redirect to same or different controller
+        //    return RedirectToRoute(new { controller = "Home", action = "About", name = "Mukesh Kumar" });
+        //}
+
+        //RedirectToActionResult in ASP.NET Core MVC
+        public RedirectToActionResult Index()
+        {
+
+            // Perform Some Logic
+            // Redirect to a different action within the same controller
+            // Specifying the Controller name is Optional
+            return RedirectToAction("About", "Home", new {name="Mukesh Kumar"});
+            //  return RedirectToAction("AboutUs");
+        }
+
+        public string About(string name)
+        {
+            return "Welcome to .Net Tutorials Test " + name;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RedirectResultsInMVC/Program.cs
@@ -0,0 +1,44 @@
+namespace RedirectResultsInMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            //URL Pattern: about
+            app.MapControllerRoute(
+                name: "AboutRoute",
+                pattern: "about",
+                defaults: new { controller = "Home", action = "about" }
+                );
+
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
